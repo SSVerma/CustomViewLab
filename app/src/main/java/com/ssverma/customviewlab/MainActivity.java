4a03267118c7593a5a2d21c34de1af1570331590
SS Verma <shyamverma1916@gmail.com>
2018-10-18 10:44:49 +0530

    Custom view interaction and configuration added

--- app/src/main/java/com/ssverma/customviewlab/MainActivity.java
+++ app/src/main/java/com/ssverma/customviewlab/MainActivity.java
@@ -1,7 +1,7 @@
 package com.ssverma.customviewlab;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +9,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpStatusView();
+    }
+
+    private void setUpStatusView() {
+        StatusView statusView = findViewById(R.id.status_view);
+
+        int totalStatus = 10;
+        int doneStatus = 4;
+
+        boolean[] status = new boolean[totalStatus];
+
+        for (int i = 0; i < doneStatus; i++) {
+            status[i] = true;
+        }
+
+        statusView.setStatusDone(status);
     }
 }
--- app/src/main/java/com/ssverma/customviewlab/StatusView.java
+++ app/src/main/java/com/ssverma/customviewlab/StatusView.java
@@ -1,6 +1,7 @@
 package com.ssverma.customviewlab;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -9,10 +10,14 @@ import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class StatusView extends View {
 
+    private static final int SHAPE_CIRCLE = 0;
+    private static final int SHAPE_SQUARE = 1;
     private boolean[] statusDone;
     private float outlineWidth;
     private float shapeSize;
@@ -23,6 +28,8 @@ public class StatusView extends View {
     private int fillColor;
     private Paint paintFill;
     private float radius;
+    private int shape;
+    private float DEFAULT_OUTLINE_WIDTH = 4;
 
     public StatusView(Context context) {
         super(context);
@@ -60,7 +67,7 @@ public class StatusView extends View {
         desiredWidth = desiredWidth + getPaddingLeft() + getPaddingRight();
 
         int rowsRequired = statusDone.length / maxHorizontalStatusIcon + 1;
-        desiredHeight = (int)((shapeSize + spacing/*Bottom spacing*/) * rowsRequired - spacing/*last row bottom spacing*/);
+        desiredHeight = (int) ((shapeSize + spacing/*Bottom spacing*/) * rowsRequired - spacing/*last row bottom spacing*/);
         desiredHeight = desiredHeight + getPaddingTop() + getPaddingBottom();
 
         int width = resolveSizeAndState(desiredWidth, widthMeasureSpec, 0);
@@ -78,14 +85,61 @@ public class StatusView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         for (int index = 0; index < statusRects.length; index++) {
-            int cx = statusRects[index].centerX();
-            int cy = statusRects[index].centerY();
+            if (shape == SHAPE_CIRCLE) {
+                int cx = statusRects[index].centerX();
+                int cy = statusRects[index].centerY();
 
-            canvas.drawCircle(cx, cy, radius, paintOutline);
-            if (statusDone[index]) {
-                canvas.drawCircle(cx, cy, radius, paintFill);
+                canvas.drawCircle(cx, cy, radius, paintOutline);
+                if (statusDone[index]) {
+                    canvas.drawCircle(cx, cy, radius, paintFill);
+                }
+
+            } else if (shape == SHAPE_SQUARE) {
+                drawSquare(canvas, index);
             }
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                int statusIconIndex = findItemAtTouchPoint(event.getX(), event.getY());
+                toggleStatusIcon(statusIconIndex);
+                return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    private void drawSquare(Canvas canvas, int statusIconIndex) {
+        Rect rect = statusRects[statusIconIndex];
+
+        if (statusDone[statusIconIndex]) {
+            canvas.drawRect(rect, paintFill);
+        }
+
+        canvas.drawRect(rect.left + outlineWidth / 2,
+                rect.top + outlineWidth / 2,
+                rect.right - outlineWidth / 2,
+                rect.bottom - outlineWidth / 2,
+                paintOutline);
+    }
+
+    private void toggleStatusIcon(int statusIconIndex) {
+        if (statusIconIndex == -1) {
+            return;
+        }
+        statusDone[statusIconIndex] = !statusDone[statusIconIndex];
+        invalidate();//Redraw -> onDraw() is called
+    }
+
+    private int findItemAtTouchPoint(float x, float y) {
+        for (int index = 0; index < statusRects.length; index++) {
+            if (statusRects[index].contains((int) x, (int) y)) {
+                return index;
+            }
+        }
+        return -1;//Invalid index
     }
 
     private void init(AttributeSet attrs) {
@@ -94,21 +148,30 @@ public class StatusView extends View {
             setUpPreviewModeValues();
         }
 
-        outlineWidth = 4f;
+        DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
+        float displayDensity = displayMetrics.density;
+        float defaultOutlineWidthPx = displayDensity * DEFAULT_OUTLINE_WIDTH;
+
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.StatusView);
+        outlineColor = typedArray.getColor(R.styleable.StatusView_outlineColor, Color.GRAY);
+        fillColor = typedArray.getColor(R.styleable.StatusView_fillColor, Color.RED);
+        outlineWidth = typedArray.getDimension(R.styleable.StatusView_outlineWidth, defaultOutlineWidthPx);
+        shape = typedArray.getInt(R.styleable.StatusView_shape, SHAPE_CIRCLE/*Circle default value*/);
+
+        typedArray.recycle();
+
         shapeSize = 144f;
         spacing = 32f;
         float diameter = shapeSize - outlineWidth;
         radius = diameter / 2;
 
         /*Outline circle*/
-        outlineColor = Color.GRAY;
         paintOutline = new Paint(Paint.ANTI_ALIAS_FLAG);
         paintOutline.setStyle(Paint.Style.STROKE);
         paintOutline.setStrokeWidth(outlineWidth);
         paintOutline.setColor(outlineColor);
 
         /*Filled circle*/
-        fillColor = Color.RED;
         paintFill = new Paint(Paint.ANTI_ALIAS_FLAG);
         paintFill.setStyle(Paint.Style.FILL);
         paintFill.setColor(fillColor);
